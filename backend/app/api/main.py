eb4985158a1948d4cd1463508d686277fdaa3c83
Varnan Jayasooriyan <89256354+598988@users.noreply.github.com>
2025-03-31 20:13:27 +0200

    Converting place to longtitude and latidude

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, utils
+from app.api.routes import items, login, private, users, utils, fire
 from app.core.config import settings
+from app.api.stedsnavn import router as stedsnavn_router
+#from app.api.frcmAPI import router as frcmapi_router
+from fastapi import FastAPI
 
 api_router = APIRouter()
 api_router.include_router(login.router)
@@ -9,6 +12,11 @@ api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(items.router)
 
+api_router.include_router(fire.fire_router)
+api_router.include_router(stedsnavn_router, prefix="/api", tags=["geonorge"])
 
 if settings.ENVIRONMENT == "local":
     api_router.include_router(private.router)
+
+
+
